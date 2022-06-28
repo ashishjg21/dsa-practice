95b3981c1c310b95f3b7292d776b3f1268be8962
ashishjg21 <ajg8188@gmail.com>
2022-06-28 23:41:17 +0530

    descrip add

---  minimum_Deletions.cpp
+++  minimum_Deletions.cpp
@@ -1,5 +1,17 @@
 // Minimum Deletions.cpp: Ashish Galagali
-// Description: .
+// Description: min
+
+//Example 1:
+// Input: s = "aab"
+// Output: 0
+// Explanation: s is already good.
+// Example 2:
+
+// Input: s = "aaabbbcc"
+// Output: 2
+// Explanation: You can delete two 'b's resulting in the good string "aaabcc".
+// Another way it to delete one 'b' and one 'c' resulting in the good string "aaabbc".
+//
 #include <iostream>
 
 using namespace std;
