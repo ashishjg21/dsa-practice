749e3b279fc2aec2fde0cc062a02cbd9a0f9046f
ashishjg21 <ajg8188@gmail.com>
2022-07-04 01:37:48 +0530

    again TODO

---
+++ chechArray_sorted_rotated.cpp
@@ -0,0 +1,29 @@
+// filename.cpp: Ashish Galagali
+// Description: .
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        
+        for(int i = 1; i<n ; i++){
+            if(nums[i-1]>nums[i])
+                count++;
+        }
+        if(nums[n-1] > nums[0])
+            count++;
+        
+        return count <= 1;
+    }
+    
+};
+int main()
+{
+    
+    check(nums);
+    return 0;
+}
+
