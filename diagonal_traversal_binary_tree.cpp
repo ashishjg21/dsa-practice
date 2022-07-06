099fa962f83e8312c8bdff4d618d1a9c47f975e1
ashishjg21 <ajg8188@gmail.com>
2022-07-06 23:35:16 +0530

    day 6dsa

---
+++ diagonal_traversal_binary_tree.cpp
@@ -0,0 +1,24 @@
+//Diagonal Traversal of Binary Tree 
+
+
+// filename.cpp: Ashish Galagali
+// Description: .
+vector<int> diagonal(Node *root)
+{
+    vector<int> v;
+    queue<Node*> q;
+    Node* p;
+    q.emplace(root);
+    while(!q.empty())
+    {    
+        p = q.front();
+        q.pop();
+        while(p)
+        {
+            if(p->left) q.emplace(p->left);
+            v.push_back(p->data);
+            p=p->right;
+        }
+    }
+    return v;
+}
